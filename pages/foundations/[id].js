0495cea1ca6e94d2bdb1763b2eff6e5d6929cb57
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-05-10 20:36:58 -0400

    broken code

--- pages/foundations/[id].js
+++ pages/foundations/[id].js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import { useRouter } from 'next/router'
+
+class FoundationDetail extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      id: null,
+      data: null,
+    }
+  }
+
+  setId = () => {
+    const id = useRouter().query.id
+    this.setState({ id: id })
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:8000/foundations/1`)
+      .then((res) => res.json)
+      .then((json) => {
+        this.setState({ data: json })
+      })
+  }
+
+  render() {
+    const foundationData = this.state.data
+    return <div> {foundationData} </div>
+  }
+}
 
 const Foundation = () => {
-  return <p> hi </p>
+  return <FoundationDetail />
 }
 
 export default Foundation
